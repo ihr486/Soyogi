7f90261e4ba1a34568f3f17f5eabd0265202d725
Hiroka Ihara <ihara_h@live.jp>
2016-01-12 20:09:40 +0900

    Fixed off-by-one bug in SRC

--- SRC.c
+++ SRC.c
@@ -3,12 +3,11 @@
 static DATA_TYPE left_in[180], right_in[180];
 static DATA_TYPE left_out[160], right_out[160];
 static int16_t audio[320];
-//static int in_pos = 16;
-static int in_pos = 0;
+static int in_pos = 16;
 
 void feed_SRC(DATA_TYPE left, DATA_TYPE right)
 {
-    /*left_in[in_pos] = left;
+    left_in[in_pos] = left;
     right_in[in_pos] = right;
     if(++in_pos == 180) {
         for(int i = 0; i < 160; i++) {
@@ -19,8 +18,8 @@ void feed_SRC(DATA_TYPE left, DATA_TYPE right)
                 right_out[i] += MUL(right_in[j + FIR_offset[i]], FIR_coeff[i][j]);
             }
         }
-        in_pos = 32;
-        for(int i = 0; i < 32; i++) {
+        in_pos = 33;
+        for(int i = 0; i < 33; i++) {
             left_in[i] = left_in[i + 147];
             right_in[i] = right_in[i + 147];
         }
@@ -29,15 +28,5 @@ void feed_SRC(DATA_TYPE left, DATA_TYPE right)
             audio[i * 2 + 1] = clamp16s((int)right_out[i]);
         }
         fwrite(audio, sizeof(int16_t) * 320, 1, sox);
-    }*/
-    left_in[in_pos] = left;
-    right_in[in_pos] = right;
-    if(++in_pos == 160) {
-        in_pos = 0;
-        for(int i = 0; i < 160; i++) {
-            audio[i * 2] = clamp16s((int)left_in[i]);
-            audio[i * 2 + 1] = clamp16s((int)right_in[i]);
-        }
-        fwrite(audio, sizeof(audio), 1, sox);
     }
 }
